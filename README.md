4f2061b434a1e4f2e3fe6c8f1af1c35eb538747d
HeavenScream <carlos.hs93@live.com.mx>
2021-06-11 11:03:49 -0500

    Inicializacion MUX

---
+++ README.md
@@ -0,0 +1,3 @@
+# MUX
+
+Developed with Unreal Engine 4
---
+++ Source/MUX/MUXGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MUXGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MUX_API AMUXGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
